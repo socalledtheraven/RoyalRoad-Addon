a10028b80b0a8938ffdc6cdb37ecf84643db28c8
QuintBrit <>
2023-01-07 21:26:35 +0000

    fix

--- chapter.js
+++ chapter.js
@@ -1,27 +1,26 @@
-var link = "https://www.royalroad.com/fiction/59918/the-bridge-to-forever-progressionlitrpg/chapter/1073612/chapter-29-the-station";
-
 function getChapter(link) {
     console.log("getting chapter");
     var chap = fetch(link)
         .then(response => response.text())
-        .then(data => {
+        .then(data => {;
             var parser = new DOMParser();
             var doc = parser.parseFromString(data, "text/html");
             // select all elements between the <hr> tags
-            var note1 = doc.querySelectorAll(".author-note-portlet")[0];
+            var note1 = doc.getElementsByClassName(".author-note-portlet");
             var chapterText = doc.querySelector(".chapter-content");
-            var note2 = doc.querySelectorAll(".author-note-portlet")[1];
+            console.log(note1);
+            var note2 = doc.querySelectorAll(".author-note-portlet");
             var next = doc.querySelector(".nav-buttons");
             var nextLink = "https://www.royalroad.com" + next.children[1].children[0].getAttribute("href");
             // combine the notes and the chapter text
+            console.log(note1.outerHTML);
             var chapter = note1.outerHTML + chapterText.outerHTML + note2.outerHTML;
-            console.log("got chapter");
-            return chapter;
-        })
-        .catch(error => console.log(error));
-    console.log("done");
-    return chap;
+            console.log(chapter);
+            return [chapter, nextLink];
+        });
 }
+
+const link = "https://www.royalroad.com/fiction/59918/the-bridge-to-forever-progressionlitrpg/chapter/1073612/chapter-29-the-station";
 
 var buttons = document.querySelector(".margin-bottom-10");
 var buttons2 = document.querySelector(".nav-buttons")
@@ -29,14 +28,12 @@ var b = buttons.children[1];
 b.removeAttribute("href");
 b.setAttribute("onclick", "getChapter()");
 b.textContent = "Full Text";
-
 buttons2.replaceWith(buttons);
 
-// var chapter = getChapter(link);
 var body = document.querySelector(".portlet-body");
 var hr = body.querySelectorAll("hr");
-console.log(hr);
-var chapter = getChapter(link);
-var img = document.createElement('img');
-img.src = 'https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png';
+console.log("test");
+var contents = getChapter(link);
+var chapter = contents[0];
+var nextLink = contents[1];
 hr[hr.length - 3].insertAdjacentHTML("afterend", chapter);
