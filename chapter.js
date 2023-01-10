ee839b2e68807c203710c70cd32f8c37b15d8e3e
QuintBrit <>
2023-01-10 22:08:04 +0000

    fixed buttons finally, so now it adds the chapter

--- chapter.js
+++ chapter.js
@@ -19,26 +19,33 @@ function fixButtons() {
 	console.log("fixing buttons");
 	var newButton = document.createElement("div");
 	newButton.setAttribute("class", "col-xs-4 col-md-4 col-lg-3 col-lg-offset-2");
+
     var subButton = document.createElement("a");
     subButton.setAttribute("class", "btn btn-primary col-xs-12");
     subButton.textContent = "Full Text";
-	newButton.addEventListener("click", function() {
-		insertNewChapter();
-	});
+	console.log("button add");
+	subButton.setAttribute("id", "runFunction");
     newButton.append(subButton);
-    console.log("new button: " + newButton.outerHTML);
+
+    // changes rr's weird button layout to one that's mostly symmetrical
 	var b1 = document.querySelector(".col-lg-offset-6");
 	b1.classList.remove("col-lg-offset-6");
     b1.classList.remove("col-md-offset-4");
     b1.classList.remove("col-xs-6");
 	b1.setAttribute("class", b1.className + " col-lg-offset-1");
 	b1.setAttribute("class", b1.className + " col-xs-4");
-	b1.insertAdjacentHTML("beforebegin", newButton.outerHTML);
+
     var b2 = document.querySelector(".col-xl-2");
     b2.classList.remove("col-xs-6");
 	b2.setAttribute("class", b2.className + " col-xs-4");
-    
+
+	b1.insertAdjacentHTML("beforebegin", newButton.outerHTML);
 }
+
+console.log("loaded chapter.js");
+fixButtons();
+
+document.getElementById("runFunction").addEventListener("click", insertNewChapter, false);
 
 async function insertNewChapter() {
 	const link = "https://www.royalroad.com/fiction/59918/the-bridge-to-forever-progressionlitrpg/chapter/1073612/chapter-29-the-station";
@@ -56,6 +63,3 @@ async function insertNewChapter() {
 	console.log("dsa");
 	hr[hr.length - 3].insertAdjacentHTML("afterend", chapter);
 }
-
-console.log("loaded chapter.js");
-fixButtons();
