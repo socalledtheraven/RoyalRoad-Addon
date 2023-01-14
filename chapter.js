f063421d159fe716f51847637ffb3f5c26bf632a
QuintBrit <>
2023-01-14 21:24:58 +0000

    IT WORKS

--- chapter.js
+++ chapter.js
@@ -2,7 +2,7 @@ function cleanHTML(html, i) {
 	var parser = new DOMParser();
 	console.log("got chapter, parsing");
 	var doc = parser.parseFromString(html, "text/html");
-	// select all elements between the <hr> tags
+
 	var titles = doc.querySelectorAll(".font-white");
 	var title = titles[titles.length - 1].textContent;
 	console.log(title);
@@ -10,6 +10,8 @@ function cleanHTML(html, i) {
 	var chapterText = doc.querySelector(".chapter-content");
 	var note2 = doc.querySelectorAll(".author-note-portlet")[1];
 	var next = doc.querySelector(".nav-buttons");
+
+	// get the next chapter link if it exists
 	try {
 		if (next.children[1].children[0].getAttribute("href") != null) {
 			var nextLink = "https://www.royalroad.com" + next.children[1].children[0].getAttribute("href");
@@ -20,8 +22,11 @@ function cleanHTML(html, i) {
 		console.log("no next chapter");
 		var nextLink = null;
 	}
+
 	// combine the notes and the chapter text
+	// if the chapter is the first one, don't add a horizontal rule
 	if (i != 1) {
+		// only add the existing notes
 		if (note1 != null && note2 != null) {
 			var chapter = "<hr>" + `<h2 class="font-black">${title}</h2>` + "<hr>" + note1.outerHTML + chapterText.outerHTML + note2.outerHTML;
 		} else if (note1 != null) {
@@ -32,6 +37,7 @@ function cleanHTML(html, i) {
 			var chapter = "<hr>" + `<h2 class="font-black">${title}</h2>` + "<hr>" + chapterText.outerHTML;
 		}
 	} else {
+		// only add the existing notes
 		if (note1 != null && note2 != null) {
 			var chapter = `<h2 class="font-black">${title}</h2>` + "<hr>" + note1.outerHTML + chapterText.outerHTML + note2.outerHTML;
 		} else if (note1 != null) {
@@ -42,8 +48,7 @@ function cleanHTML(html, i) {
 			var chapter = `<h2 class="font-black">${title}</h2>` + "<hr>" + chapterText.outerHTML;
 		}
 	}
-	// IMPORTANT: being inserted inside the 'chapter-content', fix
-	console.log("parsed chapter:" + chapter);
+	console.log("parsed chapter");
 	return [chapter, nextLink];
 }
 
@@ -75,6 +80,41 @@ function fixButtons() {
 	b1.insertAdjacentHTML("beforebegin", newButton.outerHTML);
 }
 
+function removeInitialElements() {
+	var bod = document.querySelector(".portlet-body");
+	var buttons = bod.querySelector(".nav-buttons");
+	var buttons2 = bod.querySelector(".margin-left-0");
+	var chap = bod.querySelector(".chapter-content");
+	var ad = bod.querySelector("h6.text-center");
+	var adz = bod.querySelectorAll(".wide");
+	var notes = bod.querySelectorAll(".author-note-portlet");
+	var supportNote = bod.querySelector("#donate");
+	var support = bod.querySelector("h5.margin-bottom-20");
+	var supportBar = bod.querySelectorAll(".row")[1];
+	var hrs = bod.querySelectorAll("hr");
+	var title = document.querySelector("h1.font-white");
+
+	
+	buttons.remove();
+	buttons2.remove();
+	chap.remove();
+	ad.remove();
+	adz.forEach(function(e) {
+		e.remove();
+	});
+	notes.forEach(function(e) {
+		e.remove();
+	});
+	supportNote.remove();
+	support.remove();
+	supportBar.remove();
+	// remove all the hr tags except the last one
+	for (var i = 0; i < hrs.length - 1; i++) {
+		hrs[i].remove();
+	}
+	title.remove();
+}
+
 console.log("loaded chapter.js");
 fixButtons();
 
@@ -84,32 +124,10 @@ document.getElementById("runFunction").addEventListener("click", function() {
 }, false);
 
 async function insertAllChapters() {
-	// get first chapter link
-	// grabs from the 'fiction page' button
-	var buttons = document.querySelector(".nav-buttons");
-	var buttons2 = document.querySelector(".margin-left-0");
-	var chap = document.querySelector(".portlet-body");
-	var kids = chap.children;
-	console.log(kids);
-	console.log(kids[0]);
-	for (const el in kids) {
-		if (Object.hasOwnProperty.call(kids, el)) {
-			kids[el].remove();
-		}
-	}	
-	buttons.remove();
-	buttons2.remove();
+	// removes all the normal chapter content
+	removeInitialElements();
 
-	console.log(1);
-	const storyUrl = "https://www.royalroad.com" + document.querySelector(".margin-bottom-5").getAttribute("href");
-	const response = await fetch(storyUrl);
-	var html = await response.text();
-	var parser = new DOMParser();
-	console.log("got homepage");
-	var doc = parser.parseFromString(html, "text/html");
-	console.log("aaaaaaa")
-	var firstChapterLink = "https://www.royalroad.com" + doc.querySelector(".btn-lg").getAttribute("href");
-	var nextLink = firstChapterLink;
+	var nextLink = await getFirstChapterLink();
 
 	// loop through until i hit a 404
 	var counter = 0;
@@ -138,4 +156,14 @@ async function insertNewChapter(link, i) {
 	return nextLink;
 }
 
-// ! TODO: clean up the end and refactor and document
+async function getFirstChapterLink() {
+	const storyUrl = "https://www.royalroad.com" + document.querySelector(".margin-bottom-5").getAttribute("href");
+	const response = await fetch(storyUrl);
+	var html = await response.text();
+	var parser = new DOMParser();
+	console.log("got homepage");
+	var doc = parser.parseFromString(html, "text/html");
+	var firstChapterLink = "https://www.royalroad.com" + doc.querySelector(".btn-lg").getAttribute("href");
+	return firstChapterLink;
+}
+// ! TODO: document
