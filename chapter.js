fd4318f863e77e96f1fe018f6ae52c52c8dd25d2
QuintBrit <>
2023-01-14 21:53:52 +0000

    finished documenting and logging

--- chapter.js
+++ chapter.js
@@ -5,7 +5,7 @@ function cleanHTML(html, i) {
 
 	var titles = doc.querySelectorAll(".font-white");
 	var title = titles[titles.length - 1].textContent;
-	console.log(title);
+	console.log("title of next chapter:" + title);
 	var note1 = doc.querySelectorAll(".author-note-portlet")[0];
 	var chapterText = doc.querySelector(".chapter-content");
 	var note2 = doc.querySelectorAll(".author-note-portlet")[1];
@@ -55,32 +55,34 @@ function cleanHTML(html, i) {
 function fixButtons() {
 	// grabs the buttons from the bottom of the page and turns the middle one into a "Full Text" button
 	console.log("fixing buttons");
-	var newButton = document.createElement("div");
-	newButton.setAttribute("class", "col-xs-4 col-md-4 col-lg-3 col-lg-offset-2");
+	
+	var buttonGroup = document.querySelector(".nav-buttons");
+	var buttons = buttonGroup.querySelectorAll("div");
 
-	var subButton = document.createElement("a");
-	subButton.setAttribute("class", "btn btn-primary col-xs-12");
-	subButton.innerHTML = 'Full <br class="visible-xs-block">Text';
-	console.log("button add");
-	subButton.setAttribute("id", "runFunction");
-	newButton.append(subButton);
+	// fixes both default buttons
+	for (var i = 0; i < buttons.length; i++) {
+		var b = buttons[i];
+		var link = b.children[0];
+		b.insertAdjacentElement("afterend", link);
+		b.remove();
+		link.classList.remove("col-xs-12");
+		link.setAttribute("class", link.className + " col-xs-4");
+	}
 
-	// changes rr's weird button layout to one that's mostly symmetrical
-	var b1 = document.querySelector(".col-lg-offset-6");
-	b1.classList.remove("col-lg-offset-6");
-	b1.classList.remove("col-md-offset-4");
-	b1.classList.remove("col-xs-6");
-	b1.setAttribute("class", b1.className + " col-lg-offset-1");
-	b1.setAttribute("class", b1.className + " col-xs-4");
+	// add the new button
+	var newButton = document.createElement("a");
+	newButton.setAttribute("class", "btn btn-primary col-xs-4");
+	newButton.innerHTML = 'Full <br class="visible-xs-block">Text';
+	newButton.setAttribute("id", "runFunction");
 
-	var b2 = document.querySelector(".col-xl-2");
-	b2.classList.remove("col-xs-6");
-	b2.setAttribute("class", b2.className + " col-xs-4");
-
-	b1.insertAdjacentHTML("beforebegin", newButton.outerHTML);
+	// uses the same formatting as the bottom page buttons
+	var b1 = buttonGroup.querySelector("a");
+	b1.insertAdjacentElement("afterend", newButton);
+	console.log("button addded");
 }
 
 function removeInitialElements() {
+	// removes all the original elements from the page
 	var bod = document.querySelector(".portlet-body");
 	var buttons = bod.querySelector(".nav-buttons");
 	var buttons2 = bod.querySelector(".margin-left-0");
@@ -113,6 +115,41 @@ function removeInitialElements() {
 		hrs[i].remove();
 	}
 	title.remove();
+	
+	console.log("removed initial elements");
+}
+
+async function insertNewChapter(link, i) {
+	var body = document.querySelector(".portlet-body");
+	var hr = body.querySelectorAll("hr");
+
+	// gets the actual chapter text
+	console.log("getting chapter");
+	const response = await fetch(link);
+	var html = await response.text();
+	var contents = cleanHTML(html, i);
+	var chapter = contents[0];
+	var nextLink = contents[1];
+
+	// inserts the chapter
+	var lastHr = hr[hr.length - 1];
+	lastHr.insertAdjacentHTML("beforebegin", chapter);
+	console.log("finished inserting chapter");
+	return nextLink;
+}
+
+async function getFirstChapterLink() {
+	// gets the link to the first chapter of the story via the fiction page button and the start reading button
+	const storyUrl = "https://www.royalroad.com" + document.querySelector(".margin-bottom-5").getAttribute("href");
+	const response = await fetch(storyUrl);
+	var html = await response.text();
+	var parser = new DOMParser();
+	console.log("got homepage");
+	var doc = parser.parseFromString(html, "text/html");
+	// start reading button grab
+	var firstChapterLink = "https://www.royalroad.com" + doc.querySelector(".btn-lg").getAttribute("href");
+	console.log("got first chapter link");
+	return firstChapterLink;
 }
 
 console.log("loaded chapter.js");
@@ -122,7 +159,9 @@ console.log("attaching func");
 document.getElementById("runFunction").addEventListener("click", function() {
 	insertAllChapters();
 }, false);
+console.log("attached func");
 
+// this function needs to be below for reasons of attaching the event listener
 async function insertAllChapters() {
 	// removes all the normal chapter content
 	removeInitialElements();
@@ -133,37 +172,8 @@ async function insertAllChapters() {
 	var counter = 0;
 	while (nextLink != null) {
 		counter++;
-		console.log("next link: " + nextLink);
 		nextLink = await insertNewChapter(nextLink, counter);
 	}
+	
+	console.log("end of story");
 }
-
-async function insertNewChapter(link, i) {
-	var body = document.querySelector(".portlet-body");
-	var hr = body.querySelectorAll("hr");
-
-	console.log("getting chapter");
-	const response = await fetch(link);
-	var html = await response.text();
-	var contents = cleanHTML(html, i);
-	var chapter = contents[0];
-	var nextLink = contents[1];
-
-	// console.log(chapter);
-	var lastHr = hr[hr.length - 1];
-	lastHr.insertAdjacentHTML("beforebegin", chapter);
-	console.log("finished inserting chapter");
-	return nextLink;
-}
-
-async function getFirstChapterLink() {
-	const storyUrl = "https://www.royalroad.com" + document.querySelector(".margin-bottom-5").getAttribute("href");
-	const response = await fetch(storyUrl);
-	var html = await response.text();
-	var parser = new DOMParser();
-	console.log("got homepage");
-	var doc = parser.parseFromString(html, "text/html");
-	var firstChapterLink = "https://www.royalroad.com" + doc.querySelector(".btn-lg").getAttribute("href");
-	return firstChapterLink;
-}
-// ! TODO: document
